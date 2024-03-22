812c37106c177f646a6de2d439a4c18e6bd5493a
FARIHA MALIK <163237663+farihamalik098@users.noreply.github.com>
2024-03-22 15:51:45 -0700

    ALL OPERATORS

---
+++ app.js
@@ -0,0 +1,91 @@
+//ARITHMETIC OPERATOR=
+var a = 3;
+var b = 5;
+var c = (a + b);
+console.log(c);
+//substraction
+var num1 = 5;
+var num2 = 4;
+var num = (num1 - num2);
+console.log(num);
+//multiplication
+var mul1 = 4;
+var mul2 = 5;
+var mul = (mul1 * mul2);
+console.log(mul);
+//division
+var div1 = 20;
+var div2 = 10;
+var div = (div1 / div2);
+console.log(div);
+//modulus
+var mod = 5;
+console.log(mod % 2);
+//Exponentiation
+var mango = 5;
+console.log(Math.pow(5, 2));
+//Unary operator
+//
+//postfix addition
+//ADITION
+var g = 4;
+console.log(g++);
+console.log(g++);
+//prefix(addition)
+console.log(++g);
+console.log(++g);
+//postfix substraction
+console.log(g--);
+console.log(g--);
+//prefix 
+console.log(--g);
+console.log(--g);
+//Assignment operators
+//:=,-=,+=,*=,/=...
+var i = 10;
+var j = 10;
+console.log(i = j);
+//+=
+var k = 10;
+var l = 2;
+console.log(k += l);
+//-=
+var m = 10;
+var n = 2;
+console.log(m -= n);
+//*=
+var q = 10;
+var r = 2;
+console.log(q *= r);
+// /=
+var s = 10;
+var t = 2;
+console.log(s /= t);
+//comparison operato
+//==,===,<,>,=<,>=,!=,!<>=
+//
+//==
+var shanyan = 1;
+var ayan = 2;
+console.log(shanyan == ayan);
+//===
+var A = 10;
+var B = 11;
+console.log(A === B);
+//
+var C = 10;
+var D = 11;
+console.log(A < B);
+//>
+var E = 'affan';
+var F = 'affan';
+console.log(E > F);
+//
+//logical operators
+// i.e and(&&) or(//),not(!)
+var shan = 5;
+var sha = 2;
+console.log(shan == sha && shan >= sha);
+var abc = 2;
+var bcd = 0;
+console.log(abc == bcd && bcd < abc);
---
+++ app.ts
@@ -0,0 +1,126 @@
+//ARITHMETIC OPERATOR=
+let a=3;
+let b=5;
+let c=(a+b);
+console.log(c);
+//substraction
+let num1=5;
+let num2=4;
+let num=(num1-num2);
+console.log(num);
+//multiplication
+let mul1=4;
+let mul2=5;
+let mul=(mul1 * mul2);
+console.log(mul);
+//division
+let div1=20;
+let div2=10
+let div=(div1 / div2);
+console.log(div);
+
+//modulus
+let mod=5;
+console.log(mod%2);
+
+//Exponentiation
+let mango=5;
+console.log(5**2);
+
+//Unary operator
+
+
+
+//
+//postfix addition
+//ADITION
+let g=4;
+console.log(g++);
+console.log(g++);
+//prefix(addition)
+console.log(++g);
+console.log(++g);
+
+//postfix substraction
+console.log(g--);
+console.log(g--);
+
+//prefix 
+console.log(--g);
+console.log(--g);
+
+//Assignment operators
+//:=,-=,+=,*=,/=...
+let i=10;
+let j=10;
+console.log(i=j);
+//+=
+let k=10;
+let l=2;
+console.log(k+=l);
+
+//-=
+let m=10;
+let n=2
+console.log(m-=n);
+//*=
+let q=10;
+let r=2;
+console.log(q*=r);
+
+// /=
+let s=10;
+let t=2;
+console.log(s/=t);
+
+//comparison operato
+//==,===,<,>,=<,>=,!=,!<>=
+//
+//==
+
+let shanyan=1;
+let ayan=2;
+console.log(shanyan==ayan);
+
+//===
+let A=10;
+let B=11;
+console.log(A===B);
+
+//
+let C=10;
+let D=11;
+console.log(A<B);
+
+//>
+let E='affan';
+let F='affan';
+console.log(E>F);
+
+//
+
+//logical operators
+// i.e and(&&) or(//),not(!)
+
+ 
+let shan = 5;
+let sha =2;
+console.log(shan == sha && shan >=sha);
+let abc =2;
+let bcd =0;
+console.log ( abc== bcd && bcd <abc );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
